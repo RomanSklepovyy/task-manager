44cf90250a7f156c757a1a8cb19cbc099cd0ad2d
roman <romansklepovyy1@gmail.com>
2020-08-09 23:24:13 +0300

    deleting endpoints

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ app.post('/tasks', async (req, res) => {
 })
 
 app.get('/tasks', async (req, res) => {
+
     try {
         const tasks = await Task.find({})
         res.send(tasks)
@@ -127,6 +128,37 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(404).send(e);
     }
 });
+
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(user);
+    } catch(e) {
+        res.status(500).send();
+    }
+
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(task);
+    } catch(e) {
+        res.status(500).send();
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
