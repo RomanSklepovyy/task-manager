941dd3f7ef360a7c4c09c34f2ecabef30647e365
roman <romansklepovyy1@gmail.com>
2020-08-18 18:45:03 +0300

    auth express middleware, users/me route

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
@@ -16,14 +17,8 @@ router.post('/users', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({});
-        res.status(201).send(users);
-    } catch(e) {
-        res.status(400).send(e);
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.post('/users/login', async (req, res) => {
