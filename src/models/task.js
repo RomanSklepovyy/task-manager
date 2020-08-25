54b5b7f87ab0bfdbff62825dd19f7a149bb6c683
roman <romansklepovyy1@gmail.com>
2020-08-25 16:59:18 +0300

    user - task relationships

--- src/models/task.js
+++ src/models/task.js
@@ -6,10 +6,14 @@ const Task = mongoose.model('Task', {
         required: true,
         trim: true
     },
-
     completed: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 });
 
