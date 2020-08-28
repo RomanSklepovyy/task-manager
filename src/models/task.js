bcf872de00e06b8d1ebf5c879b912f321fd504a8
roman <romansklepovyy1@gmail.com>
2020-08-28 19:52:19 +0300

    Timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
-        type: String,
         required: true,
+        type: String,
         trim: true
     },
     completed: {
@@ -15,6 +15,10 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 });
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -51,6 +52,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 });
 
 userSchema.virtual('tasks', {
@@ -95,6 +98,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user;
 };
 
+// Hash password before saving
 userSchema.pre('save', async function (next) {
     const user = this;
 
@@ -103,6 +107,13 @@ userSchema.pre('save', async function (next) {
     }
 
     next()
+});
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({owner: user._id});
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
